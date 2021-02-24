aef6e6298b26e47c1c02678684d5a3ada98e7efe
Leonardo Anjos <leodrums11@gmail.com>
2021-02-24 00:25:16 -0300

    Orm config and starting database with sqlite3

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
 
 const app = express();
 
-app.listen(8061, () => console.log('server is running...'))
+app.listen(8061, () => console.log('server is running...'));
