6df2332321c8d5c6b94b0d779f15bc2282269fbe
Leonardo Anjos <leodrums11@gmail.com>
2021-02-24 01:03:42 -0300

    Create migrations and ddl database application

---
+++ src/database/migrations/1614137786873-CreateResponses.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateResponses1614137786873 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
