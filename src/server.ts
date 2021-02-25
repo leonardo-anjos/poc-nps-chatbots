deb00f9b67dc78cbcf2bc20fbc8c5a12132f3235
Leonardo Anjos <leodrums11@gmail.com>
2021-02-24 23:55:45 -0300

    Routes and controllers - create new users

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
+
+// work with json format
+app.use(express.json());
+
+// middleware
+app.use(router);
 
 app.listen(8061, () => console.log('server is running...'));
