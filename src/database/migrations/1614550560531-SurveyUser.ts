02cdab68f5824f139ea2fdecb188ea2b152d0a7d
Leonardo Anjos <leodrums11@gmail.com>
2021-02-28 19:16:42 -0300

    Starting entities and properties from survey-user

---
+++ src/database/migrations/1614550560531-SurveyUser.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class SurveyUser1614550560531 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
