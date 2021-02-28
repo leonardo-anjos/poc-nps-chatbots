9ab90e10b5d71faeb70351e5b0b491c519ad0ccb
Leonardo Anjos <leodrums11@gmail.com>
2021-02-28 18:31:03 -0300

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { UserController } from './controller/UserController';
+import { SurveyController } from './controller/SurveyController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveyController();
 
-router.post('/users', userController.create);
+router.post('/user', userController.create);
+router.post('/survey', surveyController.create);
 
 export { router };
